2de4ba5ef2e1ad95ac30f3aaccf6a4c9269d6c00
Ignatius Qlvin <ignatiusalvin254.com>
2024-10-06 23:36:22 +0700

    memberikan warna untuk halamaan jual

--- project_market_place/lib/jual/jual.dart
+++ project_market_place/lib/jual/jual.dart
@@ -33,17 +33,18 @@ class _UploadProductScreenState extends State<UploadProductScreen> {
       barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Konfirmasi Upload'),
-          content: Text('Apakah Anda yakin ingin mengupload produk ini?'),
+          title: Text('Konfirmasi Upload', style: TextStyle(color: Colors.white)),
+          content: Text('Apakah Anda yakin ingin mengupload produk ini?', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.teal[900],
           actions: <Widget>[
             TextButton(
-              child: Text('Tidak'),
+              child: Text('Tidak', style: TextStyle(color: Colors.white)),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
             TextButton(
-              child: Text('Ya'),
+              child: Text('Ya', style: TextStyle(color: Colors.white)),
               onPressed: () {
                 Navigator.of(context).pop();
                 _navigateToReview();
@@ -74,10 +75,11 @@ class _UploadProductScreenState extends State<UploadProductScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal[100], 
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
+          icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {
             Navigator.push(
               context,
@@ -85,84 +87,164 @@ class _UploadProductScreenState extends State<UploadProductScreen> {
             );
           },
         ),
-        title: Text('Upload Produk'),
+        title: Text('Upload Produk', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.teal[900],
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.white),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(labelText: 'Nama Produk'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Harap masukkan nama produk';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _priceController,
-                decoration: InputDecoration(labelText: 'Harga Produk'),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Harap masukkan harga produk';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _descriptionController,
-                decoration: InputDecoration(labelText: 'Deskripsi Produk'),
-                maxLines: 2,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Harap masukkan deskripsi produk';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16.0),
-              Row(
-                children: <Widget>[
-                  _image != null
-                      ? Image.file(
-                          File(_image!.path),
-                          width: 200,
-                          height: 200,
-                          fit: BoxFit.cover,
-                        )
-                      : Container(
-                          width: 200,
-                          height: 200,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(12),
-                            color: Colors.grey[200],
-                          ),
-                          child: Icon(
-                            Icons.image,
-                            size: 100,
-                            color: Colors.grey[400],
-                          ),
-                        ),
-                  SizedBox(width: 16.0),
-                  ElevatedButton(
-                    onPressed: _pickImage,
-                    child: Text('Pilih Gambar'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.teal[50],
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.teal.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
                   ),
                 ],
               ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _showConfirmationDialog,
-                child: Text('Upload Produk'),
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Nama Produk',
+                      labelStyle: TextStyle(color: Colors.teal[900], fontWeight: FontWeight.bold),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal[900]!),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Harap masukkan nama produk';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: _priceController,
+                    decoration: InputDecoration(
+                      labelText: 'Harga Produk',
+                      labelStyle: TextStyle(color: Colors.teal[900], fontWeight: FontWeight.bold),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal[900]!),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Harap masukkan harga produk';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: _descriptionController,
+                    decoration: InputDecoration(
+                      labelText: 'Deskripsi Produk',
+                      labelStyle: TextStyle(color: Colors.teal[900], fontWeight: FontWeight.bold),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal[900]!),
+                      ),
+                    ),
+                    maxLines: 2,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Harap masukkan deskripsi produk';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: <Widget>[
+                      _image != null
+                          ? ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Image.file(
+                                File(_image!.path),
+                                width: 150,
+                                height: 150,
+                                fit: BoxFit.cover,
+                              ),
+                            )
+                          : Container(
+                              width: 150,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey),
+                                borderRadius: BorderRadius.circular(12),
+                                color: Colors.teal[100],
+                              ),
+                              child: Icon(
+                                Icons.image,
+                                size: 100,
+                                color: Colors.teal[400],
+                              ),
+                            ),
+                      SizedBox(width: 16.0),
+                      ElevatedButton(
+                        onPressed: _pickImage,
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                          backgroundColor: Colors.teal[900],  
+                          shadowColor: Colors.teal,  
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(Icons.image, color: Colors.white),
+                            SizedBox(width: 10),
+                            Text('Pilih Gambar', style: TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Colors.teal[800]!, Colors.teal[600]!],
+                        ),
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.teal.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 7,
+                            offset: Offset(0, 3),  
+                          ),
+                        ],
+                      ),
+                      child: ElevatedButton(
+                        onPressed: _showConfirmationDialog,
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                          backgroundColor: Colors.transparent,  
+                          shadowColor: Colors.transparent, 
+                        ),
+                        child: Text('Upload Produk', style: TextStyle(color: Colors.white, fontSize: 16)),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
